3d7229a3885173a57c0cb346ace9ce631bed1711
abhinavp403 <abhinavp403@gmail.com>
2025-01-24 18:28:10 -0500

    Target and compile sdk upgraded to 35 to fix compatibility errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "dev.abhinav.dancevideos"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.abhinav.dancevideos"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
